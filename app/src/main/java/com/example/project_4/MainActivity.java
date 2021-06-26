7e7b4a7f2523ef293b6808a9121ad3662b966e5a
Isaiah Guardado <gisaiah416@gmail.com>
2021-06-25 23:52:50 -0700

    Created a Splash Screen

--- app/src/main/java/com/example/project_4/MainActivity.java
+++ app/src/main/java/com/example/project_4/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 
@@ -16,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
+
         sign_up = findViewById(R.id.signup_button);
         sign_up.setOnClickListener(new View.OnClickListener() {
             @Override
---
+++ app/src/main/java/com/example/project_4/Splash_Activity.java
@@ -0,0 +1,27 @@
+package com.example.project_4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Splash_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        int SPLASH_TIME_OUT = 6000;
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Splash_Activity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
