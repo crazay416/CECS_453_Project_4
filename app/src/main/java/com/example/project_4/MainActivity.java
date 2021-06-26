694ac0c2bac8df183aa58f20f91851addc6208d8
Isaiah Guardado <gisaiah416@gmail.com>
2021-06-26 16:32:23 -0700

    User is now able to login to StudentLogin with credentials

--- app/src/main/java/com/example/project_4/MainActivity.java
+++ app/src/main/java/com/example/project_4/MainActivity.java
@@ -1,21 +1,34 @@
 package com.example.project_4;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
-
+    
+    
+    TextView editTextEmail;
+    TextView editTextPassword;
     Button sign_up;
     Button login;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +36,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.login_activity);
         mAuth = FirebaseAuth.getInstance();
 
+        editTextEmail = findViewById(R.id.username_id);
+        editTextPassword = findViewById(R.id.password_id);
+
         sign_up = findViewById(R.id.signup_button);
+        login = findViewById(R.id.login_button);
+
+        mAuth = FirebaseAuth.getInstance();
+
+
+
         sign_up.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -32,8 +54,53 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                userLogin();
+            }
+        });
+    }
 
+    private void userLogin() {
+        String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
 
+        if(email.isEmpty()){
+            editTextEmail.setError("Email is required!");
+            editTextEmail.requestFocus();
+            return;
+        }
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            editTextEmail.setError("Please enter a valid email");
+            editTextEmail.requestFocus();
+            return;
+        }
+
+        if(password.isEmpty()){
+            editTextPassword.setError("Password is required!");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+        if(password.length() < 6){
+            editTextPassword.setError("Size needs to be greater than 6");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    startActivity(new Intent(MainActivity.this, StudentLogin_Activity.class));
+                }
+                else{
+                    Toast.makeText(MainActivity.this, "Failed to login! Please check your credentials", Toast.LENGTH_LONG).show();
+                }
+
+            }
+        });
 
 
     }
