6548e37e73ed5b64737fd0463622e77a218ae1cc
Isaiah Guardado <gisaiah416@gmail.com>
2021-06-25 23:19:50 -0700

    Created Intent to Start SignUp_Activity

--- app/src/main/java/com/example/project_4/MainActivity.java
+++ app/src/main/java/com/example/project_4/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.project_4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button sign_up;
+    Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
+        sign_up = findViewById(R.id.signup_button);
+        sign_up.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SignUp_Activity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
     }
 }
